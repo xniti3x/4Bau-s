e7dd9da6cbe9bf0b3310f9b1f076a3a32f28c4e1
xniti3x <niti_3@hotmail.de>
2021-03-16 10:40:32 +0000

    change facicon

--- application/modules/layout/views/includes/head.php
+++ application/modules/layout/views/includes/head.php
@@ -1,4 +1,4 @@
-<title><?php echo get_setting('custom_title', null, true) ?: 'InvoicePlane';?></title>
+<title><?php echo get_setting('custom_title', null, true) ?: '4Bau-s';?></title>
 
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
